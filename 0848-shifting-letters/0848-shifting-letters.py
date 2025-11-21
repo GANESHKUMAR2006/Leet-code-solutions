8d056dc4919f934fcc9cc881be0586f376cc4d02
GANESHKUMAR2006 <meena1974r@gmail.com>
2025-11-21 21:24:46 +0530

    Time: 79 ms (72.43%), Space: 29.3 MB (54.59%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+       s=list(s)
+       tot=0
+       n=len(s)
+       for i in range(n-1,-1,-1):
+         tot+=shifts[i]
+         s[i]=chr((ord(s[i])-ord('a')+tot)%26+ord('a'))
+       return ''.join(s)
