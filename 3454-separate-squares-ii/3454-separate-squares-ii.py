5eea7e0d1c67da195d5abe9fcebf25e84e5de891
GANESHKUMAR2006 <meena1974r@gmail.com>
2026-01-14 12:48:47 +0530

    Time: 701 ms (100%), Space: 60.8 MB (94.68%) - LeetHub

--- 3454-separate-squares-ii/3454-separate-squares-ii.py
+++ 3454-separate-squares-ii/3454-separate-squares-ii.py
@@ -1,48 +1,41 @@
 class Solution:
     def separateSquares(self, squares: List[List[int]]) -> float:
-        xs, events = [], []
-        for x, y, l in squares:
-            xs += [x, x + l]
-            events.append((y, 1, x, x + l))
-            events.append((y + l, -1, x, x + l))
-        xs = sorted(set(xs))
-        events.sort()
-        idx = {v:i for i,v in enumerate(xs)}
-        n = len(xs)
-        cnt = [0] * (4*n)
-        seg = [0.0] * (4*n)
-        def update(node, l, r, ql, qr, val):
-            if qr <= l or r <= ql:
-                return
-            if ql <= l and r <= qr:
-                cnt[node] += val
+        e=[]
+        for x,y,l in squares:
+            e.append((y,1,x,x+l))
+            e.append((y+l,-1,x,x+l))
+        e.sort()
+        xs=[]
+        prev=e[0][0]
+        t=0
+        a=[]
+        def union(intervals):
+            intervals.sort()
+            res=cur=0
+            end=-10**30
+            for a,b in intervals:
+                if a>end:
+                    res+=b-a
+                    end=b
+                elif b>end:
+                    res+=b-end
+                    end=b
+            return res
+        for y,typ,x1,x2 in e:
+            if y>prev and xs:
+                h=y-prev
+                w=union(xs)
+                a.append((prev,h,w))
+                t+=h*w
+            if typ==1:
+                xs.append((x1,x2))
             else:
-                m = (l+r)//2
-                update(node*2, l, m, ql, qr, val)
-                update(node*2+1, m, r, ql, qr, val)
-
-            if cnt[node] > 0:
-                seg[node] = xs[r] - xs[l]
-            elif r - l == 1:
-                seg[node] = 0
-            else:
-                seg[node] = seg[node*2] + seg[node*2+1]
-
-        total, prevY = 0, events[0][0]
-        strips = []
-
-        for y, t, x1, x2 in events:
-            if y > prevY:
-                h = y - prevY
-                w = seg[1]
-                total += w * h
-                strips.append((prevY, h, w))
-                prevY = y
-            update(1, 0, n-1, idx[x1], idx[x2], t)
-
-        half, acc = total/2, 0
-        for y, h, w in strips:
-            if acc + h*w >= half:
-                return y + (half - acc)/w
-            acc += h*w
+                xs.remove((x1,x2))
+            prev=y
+        half=t/2
+        acc=0
+        for y,h,w in a:
+            if acc+h*w>=half:
+                return y+(half-acc)/w
+            acc+=h*w
         return 0
