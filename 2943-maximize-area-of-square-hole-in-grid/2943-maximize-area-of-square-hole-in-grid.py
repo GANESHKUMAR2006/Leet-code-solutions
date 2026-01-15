e2853ea5c07791ee6f2882b20c0bbf471073f376
GANESHKUMAR2006 <meena1974r@gmail.com>
2026-01-15 12:09:41 +0530

    Time: 0 ms (100%), Space: 19.5 MB (13.85%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
+        def area(arr):
+            arr.sort()
+            max_len=cur=1
+            for i in range(1,len(arr)):
+                if arr[i-1]+1==arr[i]:
+                    cur+=1
+                else:
+                    cur=1
+                max_len=max(max_len,cur)
+            return max_len
+        h=area(hBars)
+        v=area(vBars)
+        s=min(h,v)+1
+        return s*s
