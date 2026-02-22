90838ffc86a10fb0e68003042474e0e0d05a8487
GANESHKUMAR2006 <meena1974r@gmail.com>
2026-02-22 08:52:49 +0530

    Time: 2 ms (12.15%), Space: 19.1 MB (72.71%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -3,8 +3,8 @@ class Solution:
         n=format(n,'b')
         ans=0
         left=0
-        for i in range(1,len(n)):
-            if n[i]=='1':
-                ans=max(ans,i-left)
-                left=i
+        for right in range(1,len(n)):
+            if n[right]=='1':
+                ans=max(ans,right-left)
+                left=right
         return ans
