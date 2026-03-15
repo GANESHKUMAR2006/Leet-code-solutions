307a30fa964d50bc563475ce401cd38c9c376ec3
Ganesh Kumar R <meena1974r@gmail.com>
2026-03-15 10:33:31 +0530

    Time: 3 ms (94.59%), Space: 20.3 MB (64.54%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -6,7 +6,6 @@ class Solution:
             while alive and stack and stack[-1]>0 and asteroids[i]<0:
                 if abs(stack[-1])<abs(asteroids[i]):
                     stack.pop()
-                    continue
                 elif abs(stack[-1])==abs(asteroids[i]):
                     stack.pop()
                     alive=False
