1aa1b711120fa6e1b4e08f7b6b8956d65509d094
Ganesh Kumar R <meena1974r@gmail.com>
2026-03-21 21:53:30 +0530

    Time: 39 ms (39.67%), Space: 19.4 MB (43.39%) - LeetHub

---
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.py
@@ -0,0 +1,16 @@
+import math
+class Solution:
+    def maxLength(self, nums: List[int]) -> int:
+        n=len(nums)
+        ans=0
+        for i in range(n):
+            prod=1
+            l=1
+            g=0
+            for j in range(i,n):
+                prod*=nums[j]
+                l=math.lcm(l,nums[j])
+                g=math.gcd(g,nums[j])
+                if prod==l*g:
+                    ans=max(ans,j-i+1)
+        return ans
