21a69c5030784051a1d3dbf2cb58e40cd52a141d
Ganesh Kumar R <meena1974r@gmail.com>
2026-04-03 18:48:52 +0530

    Time: 7 ms (34.62%), Space: 20.6 MB (30.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen={}#dict
-        for i,num in enumerate(nums):
-            complement=target-num#sub the value and store in dict to see the value present in it
-            if complement in seen:
-                return [seen[complement],i]
-                break
-            seen[num]=i
+        value=[]
+        for i,val in enumerate(nums):
+            value.append((val,i))
+        nums=value
+        nums.sort()
+        left=0
+        right=len(nums)-1
+        while left<right:
+            if nums[left][0]+nums[right][0]==target:
+                return [nums[left][1],nums[right][1]]
+            elif nums[left][0]+nums[right][0]>target:
+                right-=1
+            else:
+                left+=1
