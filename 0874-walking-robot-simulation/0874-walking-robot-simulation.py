aa895178b6b92ab542cca70208dd5ba3f1e85455
Ganesh Kumar R <meena1974r@gmail.com>
2026-04-06 13:45:11 +0530

    Time: 34 ms (67.78%), Space: 23.7 MB (93.49%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        obs=set(map(tuple,obstacles))
+        x=y=0
+        d=0
+        dirs=[(0,1),(1,0),(0,-1),(-1,0)]
+        max_dist=0
+        for cmd in commands:
+            if cmd==-1:
+                d=(d+1)%4
+            elif cmd==-2:
+                d=(d-1)%4
+            else:
+                dx,dy=dirs[d]
+                for _ in range(cmd):
+                    if (x+dx,y+dy) not in obs:
+                        x+=dx
+                        y+=dy
+                        max_dist=max(max_dist,x*x+y*y)
+                    else:
+                        break
+        return max_dist
+            
