9dc1677f0a912834aaa39cbd24d01c9314dd41bd
Ganesh Kumar R <meena1974r@gmail.com>
2026-04-23 20:49:31 +0530

    Time: 387 ms (70.77%), Space: 19.3 MB (59.4%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        res=[]
+        ans=[]
         n=len(nums)
         for i in range(n-3):
             if i>0 and nums[i]==nums[i-1]:
@@ -9,17 +9,20 @@ class Solution:
             for j in range(i+1,n-2):
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                l,r=j+1,n-1
-                while l<r:
-                    s=nums[i]+nums[j]+nums[l]+nums[r]
-                    if s==target:
-                        res.append([nums[i],nums[j],nums[l],nums[r]])
-                        l+=1
-                        r-=1
-                        while l<r and nums[l]==nums[l-1]: l+=1
-                        while l<r and nums[r]==nums[r+1]: r-=1
-                    elif s<target:
-                        l+=1
+                left=j+1
+                right=n-1
+                while left<right:
+                    total=nums[i]+nums[j]+nums[left]+nums[right]
+                    if total<target:
+                        left+=1
+                    elif total>target:
+                        right-=1
                     else:
-                        r-=1
-        return res
+                        ans.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left<right and nums[left]==nums[left+1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right-1]:
+                            right-=1
+                        left+=1
+                        right-=1
+        return ans
