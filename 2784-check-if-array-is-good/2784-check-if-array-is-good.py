f5bae02a050fc7551d2daae98acd6995a4e7f75d
Ganesh Kumar R <meena1974r@gmail.com>
2026-05-14 11:36:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (59.43%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        n=len(nums)
+        res=[0]*(n)
+        for a in nums:
+            if a>=n:
+                return False
+            if a<n-1 and res[a]>0:
+                return False
+            res[a]+=1
+        return res[n-1]==2
