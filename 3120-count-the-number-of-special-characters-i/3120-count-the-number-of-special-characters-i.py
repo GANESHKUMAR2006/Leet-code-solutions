c53aecd82c36fec3a8fd54cc23990de4f6b8d2fe
Ganesh Kumar R <meena1974r@gmail.com>
2026-05-26 10:25:52 +0530

    Time: 1 ms (47.62%), Space: 19.2 MB (64.16%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,7 +1,16 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
+        res=[0]*(26)
+        upper=lower=set()
+        for c in word:
+            if ord(c)>96 and c not in upper:
+                res[97-ord(c)]+=1
+                upper.add(c)
+            if ord(c)<91 and c not in lower:
+                res[65-ord(c)]+=1
+                lower.add(c)
         count=0
-        for w in set(word):
-            if w.islower() and w.upper() in set(word):
+        for num in res:
+            if num==2:
                 count+=1
         return count
