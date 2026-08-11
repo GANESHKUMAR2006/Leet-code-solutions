c55317df1ffe7a0eb96b3bf1da3c4201e2020d2d
Ganesh Kumar R <meena1974r@gmail.com>
2026-08-11 10:08:21 +0530

    Time: 7 ms (56.42%), Space: 21.1 MB (8.09%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        @lru_cache(None)
+        def dp(i,j):
+            if j==len(p):
+                return i==len(s)
+            first=(i<len(s) and (s[i]==p[j] or p[j]=='.'))
+            if j+1<len(p) and p[j+1]=='*':
+                skip=dp(i,j+2)
+                use=first and dp(i+1,j)
+                return skip or use
+            return first and dp(i+1,j+1)
+        return dp(0,0)
